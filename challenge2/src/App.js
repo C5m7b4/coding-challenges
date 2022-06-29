e3b2269ff3177ea15f63e37bd77583383b72b4bd
C5m7b4 <mbedingfield@dcrpos.com>
2022-06-29 12:55:33 -0500

    Add challenge2

---
+++ challenge2/src/App.js
@@ -0,0 +1,8 @@
+import RobotList from './components/RobotList';
+import './App.css';
+
+function App() {
+  return <RobotList />;
+}
+
+export default App;
---
+++ challenge2/src/components/RobotList.js
@@ -0,0 +1,9 @@
+const RobotList = () => {
+  return (
+    <div>
+      <h2>Robot List</h2>
+    </div>
+  );
+};
+
+export default RobotList;
